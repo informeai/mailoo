3fe459d9de10498c242e67c57e758925902a488a
Wellington Gadelha <wellingtongadelha@Informeai.local>
2020-03-30 12:06:05 -0300

    Add docstring to format international.

--- mailoo/mailoo.py
+++ mailoo/mailoo.py
@@ -5,18 +5,18 @@ from email.mime.text import MIMEText
 
 class Mail:
     """
-    Classe em Python para criaçao de Emails e envios via servidores SMTPs...
-    Uso da Classe:
-    Se o Email do Usuário for do GMAIL -> Necessario criar chave de aplicativo , Link: https://support.google.com/mail/answer/185833?hl=pt-BR, 
-    de posse da chave , usar como a senha.
-    Se o Email do Usuário for do OUTLOOK,HOTMAIL,YAHOO -> Usar email e senha padrao.
+    Python class for creating Emails and sending via SMTP servers ...
+    Class Use:
+    If the User Email is from GMAIL -> Necessary to create application key, Link: https://support.google.com/mail/answer/185833?hl=en,
+    in possession of the key, use as the password.
+    If the User Email is from OUTLOOK, HOTMAIL, YAHOO -> Use default email and password.
 
     """
 
     def __init__(self,user_email:str, key_acess:str):
         """
-        Metodo de Inicializaçao do Objeto Email...
-        Cria uma instancia do Objeto Mail.
+        Email Object Initialization Method ...
+        Creates an instance of the Mail Object.
         """
         self.user_email = user_email
         self.key_acess = key_acess
@@ -25,19 +25,19 @@ class Mail:
 
     def send(self,subject:str,msg:str=None,email:str=None,file_html:str=None,file_emails:str=None):
         """
-        Método responsavel por enviar emails...
-        USO: 
-        msg ou file_html -> mensagem a ser enviada/arquivo html a ser enviado.
-        email ou file_emails -> email unico de destinatário/arquivo 'TXT', contendo emails.
-        subject -> Assunto do Email.
+        Method responsible for sending emails ...
+        USE:
+        msg or file_html -> message to be sent / html file to be sent.
+        email or file_emails -> single recipient email / file 'TXT', containing emails.
+        subject -> Email Subject.
 
         """
         if msg and file_html or email and file_emails:
-            print(f'Duplicidade de Argumentos : USO -> msg ou file_html, emails ou file_emails')
+            print(f'Duplicated of Arguments : USE -> msg ou file_html, emails ou file_emails')
         else:
 
             if not msg and not file_html:
-                print(f'Escreva uma mensagem ou envie um arquivo html.')
+                print(f'Write a message or send an html file.')
 
             if file_emails and msg:
                 list_emails_msg = self._load_emails(file_emails)
@@ -56,7 +56,7 @@ class Mail:
 
     def _send_file(self, file, subject:str,dest_email:str):
         """
-        Metodo Usado para envio de Arquivos. Ex: *.html, *.txt.
+        Method Used for sending files. Ex: * .html, * .txt.
         """
         self.file = file
         
@@ -65,9 +65,9 @@ class Mail:
                 content_html = arquivo.read()
                 arquivo.close()
         except FileNotFoundError:
-            print(f'ARQUIVO: \033[1;91m{self.file}\033[0;0m4 não Existe.\n')
+            print(f'FILE: \033[1;91m{self.file}\033[0;0m4 not Existed.\n')
         else:
-            print(f'Arquivo Lido com Sucesso.\n')
+            print(f'File Read Successfully.\n')
             
 
         self.msg = content_html
@@ -85,21 +85,21 @@ class Mail:
             except Exception as err:
                 print(f'ERROR: {err}')
             else:
-                print(f'Email enviado ao DESTINATARIO: \033[1;32m{self.dest_email}\033[0;0m  concluido.')
+                print(f'Email sent to the RECIPIENT: \033[1;32m{self.dest_email}\033[0;0m  done.')
         else:
             with SMTP_SSL(self.host, self.port) as mail:
                 try:
                     mail.login(self.user_email,self.key_acess)
                 except SMTPAuthenticationError:
-                    print(f'O Servidor não aceitou a combinação do USUÁRIO: {self.user_email} , mais a SENHA')
+                    print(f'The Server did not accept the USER combination: {self.user_email} , more the PASSWORD')
                 except SMTPHeloError:
-                    print(f'O Servidor de Email não respondeu a solicitação de login.')
+                    print(f'The Email Server did not respond to the login request.')
                 except SMTPServerDisconnected:
-                    print(f'O Servidor Desconectou Inesperadamente.')
+                    print(f'The Server Unexpectedly Disconnected.')
                 except SMTPResponseException as resp_err:
-                    print(f'Houve um erro com a resposta do servidor.\n CODE: {resp_err.smtp_code}.\n ERROR: {resp_err.smtp_error}')
+                    print(f'There was an error with the server response.\n CODE: {resp_err.smtp_code}.\n ERROR: {resp_err.smtp_error}')
                 except SMTPException:
-                    print(f'Nenhum método de Authenticação encontrado.')
+                    print(f'No Authentication method found.')
                 
                 try:
                     mail.send_message(self.msg,self.user_email,self.dest_email)
@@ -107,11 +107,11 @@ class Mail:
                 except Exception as err:
                     print(err)
                 else:
-                    print(f'Email enviado ao DESTINATARIO: \033[1;32m{self.dest_email}\033[0;0m concluido')
+                    print(f'Email sent to the RECIPIENT: \033[1;32m{self.dest_email}\033[0;0m done.')
 
     def _send_text(self, text, subject:str,dest_email:str):
         """
-        Metodo Usado para envio de Texto simple ou html in-line.
+       Method Used to send plain text or html in-line.
         """
         self.msg = text
         self.subject = subject
@@ -131,21 +131,21 @@ class Mail:
             except Exception as err:
                 print(f'ERROR: {err}')
             else:
-                print(f'Email enviado ao DESTINATARIO: \033[1;32m{self.dest_email}\033[0;0m  concluido.')
+                print(f'Email sent to the RECIPIENT: \033[1;32m{self.dest_email}\033[0;0m  done.')
         else:
             with SMTP_SSL(self.host, self.port) as mail:
                 try:
                     mail.login(self.user_email,self.key_acess)
                 except SMTPAuthenticationError:
-                    print(f'O Servidor não aceitou a combinação do USUÁRIO: {self.user_email} , mais a SENHA')
+                    print(f'The Server did not accept the USER combination: {self.user_email} , more the PASSWORD.')
                 except SMTPHeloError:
-                    print(f'O Servidor de Email não respondeu a solicitação de login.')
+                    print(f'The Email Server did not respond to the login request.')
                 except SMTPServerDisconnected:
-                    print(f'O Servidor Desconectou Inesperadamente.')
+                    print(f'The server unexpectedly disconnected.')
                 except SMTPResponseException as resp_err:
-                    print(f'Houve um erro com a resposta do servidor.\n CODE: {resp_err.smtp_code}.\n ERROR: {resp_err.smtp_error}')
+                    print(f'There was an error with the server response.\n CODE: {resp_err.smtp_code}.\n ERROR: {resp_err.smtp_error}')
                 except SMTPException:
-                    print(f'Nenhum método de Authenticação encontrado.')
+                    print(f'No Authentication method found.')
                     
                 try:
                     mail.send_message(self.msg,self.user_email,self.dest_email)
@@ -153,7 +153,7 @@ class Mail:
                 except Exception as err:
                     print(err)
                 else:
-                    print(f'Email enviado ao DESTINATARIO: \033[1;32m{self.dest_email}\033[0;0m concluido')
+                    print(f'Email sent to the RECIPIENT: \033[1;32m{self.dest_email}\033[0;0m done.')
 
     def _html(self, msg, dest_email,subject=None):
         self.msg = msg
@@ -189,8 +189,8 @@ class Mail:
                     lista_emails.append(email)
             arq_emails.close()
         except FileNotFoundError:
-            print(f'Arquivo:  \033[1;91m{file}\033[0;0m nao reconhecido')
+            print(f'File:  \033[1;91m{file}\033[0;0m unrecognized.')
         else:
-            print(f'Lista de Arquivos de Emails Carregado.')
+            print(f'List of Uploaded Email Files.')
             return lista_emails
         
